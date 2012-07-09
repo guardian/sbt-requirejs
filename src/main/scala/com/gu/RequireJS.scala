cec3ceb1e212730d33c8878122e06d34625f8d02
Grant Klopper <grant.klopper@guardian.co.uk>
2012-07-09 14:49:34 +0100

    First version that can considered to be working :)

--- src/main/scala/com/gu/RequireJS.scala
+++ src/main/scala/com/gu/RequireJS.scala
@@ -3,9 +3,6 @@ package com.gu
 import java.io.File
 import sbt._
 import sbt.Keys._
-import org.mozilla.javascript.tools.shell.Main
-import io.Source
-import com.codahale.jerkson.Json._
 
 object RequireJS extends Plugin {
 
@@ -13,38 +10,21 @@ object RequireJS extends Plugin {
   val requireJsDir = SettingKey[File]("require-js-dir", "The location you want the javascript optimized to")
   val requireJsBaseUrl = SettingKey[String]("require-js-base-url", "The base url of requireJs modules")
 
-  val requireJsRJSFile = SettingKey[File]("require-js-rjs-file", "The r.js file that is used to compile requirejs files")
+  //val requireJsRJSFile = SettingKey[File]("require-js-rjs-file", "The r.js file that is used to compile requirejs files")
 
   val requireJsModules = SettingKey[Seq[String]]("require-js-modules", "The requireJs entry modules (usually main - for main.js)")
 
 
-  val requireJsSettings = Seq[Project.Setting[_]](
-    requireJsRJSFile := writeRJSFile
-  )
+  val requireJsSettings = Seq[Project.Setting[_]]()
 
-  private def writeRJSFile = {
-    val rjsFile = IO.createTemporaryDirectory / "r.js"
-    val rjs = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("r.js")).mkString
-    IO.write(rjsFile, rjs)
-    rjsFile
-  }
+  def requireJsCompiler = (requireJsAppDir, requireJsDir, requireJsBaseUrl, requireJsModules, streams) map {
+    (appDir, dir, baseUrl, modules, s) =>
+      implicit val log = s.log
 
-  private case class Module(name: String)
-  private case class RequireJsConfig(baseUrl: String, appDir: String, dir: String, modules: Seq[Module])
-
-  def requireJsCompiler = (requireJsRJSFile, requireJsAppDir, requireJsDir, requireJsBaseUrl, requireJsModules, streams) map {
-    (rjsFile, appDir, dir, baseUrl, modules, s) =>
-      import s.log
-
-      //using the file based config, as the arguments based config does not seem to support multiple modules...
-      val config = generate(RequireJsConfig(baseUrl, appDir.getAbsolutePath, dir.getAbsolutePath, modules.map(Module(_))))
-      log.info("Running requirejs optimization with config: " + config)
-
-      val configFile = IO.createTemporaryDirectory / "config.js"
-      IO.write(configFile, config)
-
-      //Main.main(Array(rjsFile.getAbsolutePath, "-o", "baseUrl=" + baseUrl, "appDir=" + appDir, "dir=" + dir, "modules=name:'main',name:'after'"))
-      Main.main(Array(rjsFile.getAbsolutePath, "-o", configFile.getAbsolutePath))
-      Seq.empty[File]
+      val config = RequireJsConfig(baseUrl, appDir.getAbsolutePath, dir.getAbsolutePath, modules.map(Module(_)))
+      RequireJsOptimizer.optimize(config)
   }
 }
+
+
+
---
+++ src/main/scala/com/gu/RequireJsOptimizer.scala
@@ -0,0 +1,48 @@
+package com.gu
+
+import sbt._
+import io.Source
+import org.mozilla.javascript.tools.shell.{Global, ShellContextFactory, Main}
+import com.codahale.jerkson.Json._
+
+case class Module(name: String)
+case class RequireJsConfig(baseUrl: String, appDir: String, dir: String, modules: Seq[Module])
+
+object RequireJsOptimizer {
+
+  private val rjsFile = {
+    val dir = IO.createTemporaryDirectory
+    dir.deleteOnExit()
+    val rjsFile = dir / "r.js"
+    val rjs = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("r.js")).mkString
+    IO.write(rjsFile, rjs)
+    rjsFile
+  }
+
+  def optimize(config: RequireJsConfig)(implicit log: Logger) = {
+
+    val configFile = IO.createTemporaryDirectory / "config.js"
+    val configString = generate(config)
+
+    log.info("Running requirejs optimization with config: " + configString)
+
+    IO.write(configFile, configString)
+
+    //if we do not do this then the script is not executed again
+    //I found this out by trial and error
+    Main.shellContextFactory = new ShellContextFactory()
+    Main.global = new Global()
+
+    val result = Main.exec(Array(rjsFile.getAbsolutePath, "-o", configFile.getAbsolutePath))
+
+    //TODO find a case where this happens and figure out what to do about it
+    if (result != 0) {
+      System.exit(result)
+    }
+
+    val allJsFiles = (file(config.appDir) ** "*.js").get
+    val destination = file(config.dir)
+    val optimizedFiles = (allJsFiles x rebase(file(config.appDir), destination)) map (_._2)
+    optimizedFiles
+  }
+}
