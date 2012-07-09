ed91150afad3bfc68817fc64c4cc318fd830aa0d
Grant Klopper <grant.klopper@guardian.co.uk>
2012-07-09 16:41:05 +0100

    Added timing and ability to turn off optimization

--- src/main/scala/com/gu/RequireJS.scala
+++ src/main/scala/com/gu/RequireJS.scala
@@ -9,19 +9,21 @@ object RequireJS extends Plugin {
   val requireJsAppDir = SettingKey[File]("require-js-app-dir", "The location of the javascript you want to optimize")
   val requireJsDir = SettingKey[File]("require-js-dir", "The location you want the javascript optimized to")
   val requireJsBaseUrl = SettingKey[String]("require-js-base-url", "The base url of requireJs modules")
+  val requireJsOptimize = SettingKey[Boolean]("require-js-optimize", "Let requireJs know whether to optimize files or not")
 
   //val requireJsRJSFile = SettingKey[File]("require-js-rjs-file", "The r.js file that is used to compile requirejs files")
 
   val requireJsModules = SettingKey[Seq[String]]("require-js-modules", "The requireJs entry modules (usually main - for main.js)")
 
-
-  val requireJsSettings = Seq[Project.Setting[_]]()
-
-  def requireJsCompiler = (requireJsAppDir, requireJsDir, requireJsBaseUrl, requireJsModules, streams) map {
-    (appDir, dir, baseUrl, modules, s) =>
+  def requireJsCompiler = (requireJsOptimize, requireJsAppDir, requireJsDir, requireJsBaseUrl, requireJsModules, streams) map {
+    (optimize, appDir, dir, baseUrl, modules, s) =>
       implicit val log = s.log
 
-      val config = RequireJsConfig(baseUrl, appDir.getAbsolutePath, dir.getAbsolutePath, modules.map(Module(_)))
+      val optimizeOpt = if (optimize) None else Some("none")
+
+      val config = RequireJsConfig(baseUrl, appDir.getAbsolutePath,
+        dir.getAbsolutePath, modules.map(Module(_)), optimizeOpt)
+
       RequireJsOptimizer.optimize(config)
   }
 }
--- src/main/scala/com/gu/RequireJsOptimizer.scala
+++ src/main/scala/com/gu/RequireJsOptimizer.scala
@@ -6,7 +6,11 @@ import org.mozilla.javascript.tools.shell.{Global, ShellContextFactory, Main}
 import com.codahale.jerkson.Json._
 
 case class Module(name: String)
-case class RequireJsConfig(baseUrl: String, appDir: String, dir: String, modules: Seq[Module])
+
+case class RequireJsConfig(baseUrl: String, appDir: String, dir: String, modules: Seq[Module],
+                           // to turn off optimization use optimize=Some("none")    - yeah, I know
+                           optimize: Option[String])
+
 
 object RequireJsOptimizer {
 
@@ -20,29 +24,37 @@ object RequireJsOptimizer {
   }
 
   def optimize(config: RequireJsConfig)(implicit log: Logger) = {
+    time{
+      val configFile = IO.createTemporaryDirectory / "config.js"
+      val configString = generate(config)
 
-    val configFile = IO.createTemporaryDirectory / "config.js"
-    val configString = generate(config)
+      log.info("Running requirejs optimization with config: " + configString)
 
-    log.info("Running requirejs optimization with config: " + configString)
+      IO.write(configFile, configString)
 
-    IO.write(configFile, configString)
+      //if we do not do this then the script is not executed again
+      //I found this out by trial and error
+      Main.shellContextFactory = new ShellContextFactory()
+      Main.global = new Global()
 
-    //if we do not do this then the script is not executed again
-    //I found this out by trial and error
-    Main.shellContextFactory = new ShellContextFactory()
-    Main.global = new Global()
+      val result = Main.exec(Array(rjsFile.getAbsolutePath, "-o", configFile.getAbsolutePath))
 
-    val result = Main.exec(Array(rjsFile.getAbsolutePath, "-o", configFile.getAbsolutePath))
+      //TODO find a case where this happens and figure out what to do about it
+      if (result != 0) {
+        System.exit(result)
+      }
 
-    //TODO find a case where this happens and figure out what to do about it
-    if (result != 0) {
-      System.exit(result)
+      val allJsFiles = (file(config.appDir) ** "*.js").get
+      val destination = file(config.dir)
+      val optimizedFiles = (allJsFiles x rebase(file(config.appDir), destination)) map (_._2)
+      optimizedFiles
     }
+  }
 
-    val allJsFiles = (file(config.appDir) ** "*.js").get
-    val destination = file(config.dir)
-    val optimizedFiles = (allJsFiles x rebase(file(config.appDir), destination)) map (_._2)
-    optimizedFiles
+  private def time[A](block: => A)(implicit log: Logger): A = {
+    val start = System.currentTimeMillis
+    val result = block
+    log.info("RequireJs optimization took " + (System.currentTimeMillis() - start) + " ms")
+    result
   }
 }
